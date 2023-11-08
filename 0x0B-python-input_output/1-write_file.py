f770361560a9f93650e3b80ec49a9429af69786d
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-11-07 18:16:14 -0800

    check6

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" write file"""
+""" write file """
 
 
 def write_file(filename="", text=""):
-    """function"""
+    """ function """
     with open(filename, 'w', encoding="utf-8") as f:
             f.write(text)
             return len(text)

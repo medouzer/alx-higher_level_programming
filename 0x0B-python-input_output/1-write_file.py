f7dee0e3c59ccd0d1318dee464a6da3e094540db
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-11-07 18:12:58 -0800

    tasl 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" write file"""
+
+
+def write_file(filename="", text=""):
+    """function"""
+    with open(filename, 'w', encoding="utf-8") as f:
+            f .write(text)
+            return len(text)

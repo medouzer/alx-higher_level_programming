a94bf048c171b346a2d3d93db1c176ce7e34853e
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-11-08 18:56:51 -0800

    last tasks

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ the class """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size

0cadec9349c2e807b97df59466a77675992c86b4
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-11-08 18:09:04 -0800

    fix the task 0 and make task 2

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ available attributes and methods of an object"""
 
 
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" exact same object """
+
+
+def is_same_class(obj, a_class):
+    """ function """
+    return type(obj) is a_class

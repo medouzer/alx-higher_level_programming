b71e8c4dc9e8e91f093345fae499e4b994612ffd
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-12 14:11:11 -0800

    task 1 and 2

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- script that creates the database hbtn_0c_0
+-- connect with mysql -uroot -p
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- script that deletes the database hbtn_0c_0
+-- conect with mysql -uroot -p
+DROP DATABASE IF EXISTS hbtn_0c_0

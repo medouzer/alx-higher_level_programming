2fbccc7d438be097b5cc73dfa3e7264313952da4
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-12 15:24:48 -0800

    task 6, 7 and 8

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- script that lists all rows of the table first_table
+-- conect with mysql -uroot -p
+SELECT * FROM first_table;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- script that displays the number of records with id = 89
+-- conect with mysql -uroot -p
+SELECT COUNT(*) FROM first_table WHERE id=89;

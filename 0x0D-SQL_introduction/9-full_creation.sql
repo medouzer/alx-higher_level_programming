339b47fcd6ea54a2d09581c54a6b19481c26d7ef
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-12 16:41:44 -0800

    fix errors

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- conect with mysql -uroot -p
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(2, 'Bob', 14),
-(4, 'George', 8);
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(2, 'Bob', 14),
+	(4, 'George', 8);

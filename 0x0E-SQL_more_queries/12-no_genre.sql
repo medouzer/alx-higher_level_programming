d2e5230a1938c4afda16342728cba2cc8dff8d7a
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-12-14 07:56:58 -0800

    fix error

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
-select s.title, sg.genre_id FROM tv_shows s LEFT JOIN tv_show_genres sg ON s.id = sg.show_id WHERE sg.genre_id IS NULL ORDER BY s.title, sg.genre_id ASC;
+SELECT s.title, sg.genre_id FROM tv_shows s LEFT JOIN tv_show_genres sg ON s.id = sg.show_id WHERE sg.genre_id IS NULL ORDER BY s.title, sg.genre_id ASC;

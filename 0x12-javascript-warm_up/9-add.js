a2c6caf4bbab3bf185a21f8a4335110016664ed9
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-09 03:25:19 +0100

    check 4

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-const nb1 = parseInt(argv[2]);
-const nb2 = parseInt(argv[3]);
 function add(a, b) {
   return a + b;
 }
+const { argv } = require('process');
+const nb1 = parseInt(argv[2], 10);
+const nb2 = parseInt(argv[3], 10);
 console.log(add(nb1, nb2));

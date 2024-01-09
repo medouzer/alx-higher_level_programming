9c9f6bbd2d83258bb9ad250c47d160a6c10851a8
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-09 03:21:38 +0100

    the checker

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -6,5 +6,4 @@ function add(a, b) {
 const { argv } = require('process');
 const nb1 = parseInt(argv[2]);
 const nb2 = parseInt(argv[3]);
-const sum = add(nb1, nb2);
-console.log(sum);
+console.log(add(nb1, nb2));

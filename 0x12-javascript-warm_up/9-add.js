8b7c975f7f780114c4a857a3271874ec3844e087
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-09 03:18:22 +0100

    ahhh

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+
 function add(a, b) {
   return a + b;
 }
-
-const { argv } = require('process');
-
 const nb1 = parseInt(argv[2]);
 const nb2 = parseInt(argv[3]);
 const sum = add(nb1, nb2);
-
 console.log(sum);

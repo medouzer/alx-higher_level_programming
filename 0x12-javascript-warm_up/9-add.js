3e0e57d05096dec6486c5ce380123db0dbafef22
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-09 03:15:53 +0100

    the check is ...

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,11 @@
 function add(a, b) {
   return a + b;
 }
+
 const { argv } = require('process');
 
-const a = parseInt(argv[2]);
-const b = parseInt(argv[3]);
-const sum = add(a, b);
+const nb1 = parseInt(argv[2]);
+const nb2 = parseInt(argv[3]);
+const sum = add(nb1, nb2);
 
 console.log(sum);

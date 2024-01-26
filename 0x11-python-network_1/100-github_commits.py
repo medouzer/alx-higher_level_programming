d8a5e0591d0c236c82519dcf97134d7c24a097ca
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-27 00:51:07 +0100

    lala

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,8 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits?per_page=10"
+    url = f"https://api.github.com/repos/{owner_name}\
+/{repo_name}/commits?per_page=10"
     # headers = {'Authorization': 'Bearer {}'.format(password)}
     req = requests.get(url).json()
     for i in req:

138b323f943a8a268a85edd13c9e751746b17b57
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 22:26:38 +0100

    test

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Error code #1"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    if (req.status_code >= 400):
+        print(f"Error code: {req.status_code}")
+    else:
+        print(req.text)

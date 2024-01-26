0e796d49fef3bdbe860d9aa4bf2128989c30a971
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-27 00:18:30 +0100

    to tests2

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,8 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    url = f"https://api.github.com/{username}"
-    headers = {'Authorization': 'Bearer {}'.format(password)}
-    req = requests.get(url, headers=headers)
+    url = f"https://api.github.com/users/{username}"
+    # print(url)
+    # headers = {'Authorization': 'Bearer {}'.format(password)}
+    req = requests.get(url, auth=(username, password))
     print(req.json().get('id'))

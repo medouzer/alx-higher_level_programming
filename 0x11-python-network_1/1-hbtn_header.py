865ff534e83d5a63d81fd2884f9ed44240dcde04
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 19:13:31 +0100

    test

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """Response header value #0"""
-import urllib
+import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.args
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        info = response.info()
+        print(f"info: {info}")

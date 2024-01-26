9771dc4bd15752ff8390c3bca8cd248b7269e06a
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 13:13:10 -0800

    task

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,4 +7,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    print(req.headers.get()["X-Request-Id"])
+    info = req.headers.get("X-Request-Id")
+    print(info)

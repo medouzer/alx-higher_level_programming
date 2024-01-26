e1c00bccfaaa47b3ca4fd7a0e7dc1ffea57ae68e
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 13:05:37 -0800

    new task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     req = requests.get(url)
     print("Body response:")
-    print(f"\t- type: {type(req)}")
+    print(f"\t- type: {type(req.text)}")
     print(f"\t- content: {req.text}")

b55b514918d9ec737027d5e90335c4e1c20a1479
ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 22:40:11 +0100

    to test

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Search API"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    try:
+        url = 'http://0.0.0.0:5000/search_user'
+        req = requests.post(url, data={'q': q})
+        data_json = req.json()
+        if data_json:
+            print(f"[{data_json["id"]}] {data_json["name"]}")
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")

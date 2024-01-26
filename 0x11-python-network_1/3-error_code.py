c50f153f9c9d0075e62949b4ffa3e09352825b14
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 10:53:03 -0800

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
             de_respons = respons.read().decode('utf-8')
             print(de_respons)
     except urllib.error.HTTPError as e:
-        print(f"Error code: {e}")
+        print(f"Error code: {e.code}")

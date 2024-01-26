9720f47de8f8c9ff88106223c3f54f87c84f7b81
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-01-26 09:55:23 -0800

    ggg

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as respons:
         body = respons.read()
-        print("Body response:$")
-        print(f"\t- type: {type(body)}$")
-        print(f"\t- content: {body}$")
+        print(f"Body response:$\n\t- type: {type(body)}$\n\t- content: {body}$")
         print(f"\t- utf8 content: {body.decode('utf8')}$")

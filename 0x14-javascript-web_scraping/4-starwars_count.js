6576a2176c828f27a4e4faa700ab008bc3163da9
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-20 19:34:42 +0100

    new things

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,17 +8,14 @@ request.get(url, { json: true }, (err, code, data) => {
     return;
   }
   let count = 0;
-  if (code.statusCode == 200) {
-    const results = data.results;
-    results.forEach((film) => {
-      if (
-        film.characters.includes(
-          'https://swapi-api.alx-tools.com/api/people/18/'
-        )
-      ) {
-        count++;
-      }
-    });
-    console.log(count);
-  }
+  console.log(code.statusCode);
+  const results = data.results;
+  results.forEach((film) => {
+    if (
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    ) {
+      count++;
+    }
+  });
+  console.log(count);
 });
